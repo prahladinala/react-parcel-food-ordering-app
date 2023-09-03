738a4771a0842bc359ce6018aa7a252024d6e27f
prahladinala <prahlad2018@gmail.com>
2023-09-03 15:03:43 +0530

    named export ⇢ constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-const CDN_URL =
+export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-const LOGO_URL = "../logo-v.png";
+export const LOGO_URL = "../logo-v.png";
