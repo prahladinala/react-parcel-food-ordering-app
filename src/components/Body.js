bb384822d21a8866943c7e6811e5481cd5b8ec14
prahladinala <prahlad2018@gmail.com>
2023-09-03 23:19:42 +0530

    Shimmer UI Conditional Rendering 😎

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,11 +23,13 @@ const Body = () => {
     );
   };
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
