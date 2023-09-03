032bf21101de72a5d4b72302f206abf6ee4b43f5
prahladinala <prahlad2018@gmail.com>
2023-09-03 15:05:38 +0530

    named import ⇢ RestaurantCard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants";
+
 const RestaurantCard = (props) => {
   // console.log(props) // JS Object
   const { resData } = props;
@@ -5,14 +7,7 @@ const RestaurantCard = (props) => {
     resData?.info;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="res-logo"
-        alt={name}
-        src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          cloudinaryImageId
-        }
-      />
+      <img className="res-logo" alt={name} src={CDN_URL + cloudinaryImageId} />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} Stars</h4>
