c89c6758ba2e7a9c0b23549789ea610b6ba99232
prahladinala <prahlad2018@gmail.com>
2023-09-03 15:21:18 +0530

    Footer Component separted 💪

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Heading from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
-const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  );
-};
 const AppLayout = () => {
   return (
     <div className="app">
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const styling = {
+  backgroundColor: "#ce2627",
+  color: "#ffffff",
+  width: "100%",
+  padding: "8px",
+  marginTop: "10px",
+};
+const Footer = () => {
+  return (
+    <div style={styling}>
+      <h3>© 2023 ToolMate, Inc. All right reserved.</h3>
+    </div>
+  );
+};
+export default Footer;
