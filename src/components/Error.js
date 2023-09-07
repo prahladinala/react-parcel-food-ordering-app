73bb0431f231e7386e3b1422017b3af7778d57de
Prahlad <prahladjngp@gmail.com>
2023-09-07 06:54:44 +0530

    useRouteError in React Router DOM - ❌❌❌

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,13 +1,18 @@
 import Footer from "./Footer";
 import Heading from "./Header";
-
+import { useRouteError } from "react-router-dom";
 const Error = () => {
+  const err = useRouteError();
+  console.log(err);
   return (
     <div>
       <Heading />
       <div className="align-center container">
-        <h1>Oops! 404 Page Not Found</h1>
+        <h1>
+          Oops! {err.status} Page {err.statusText}
+        </h1>
         <h2>Something went wrong</h2>
+        <p>{err.data}</p>
       </div>
       <Footer />
     </div>
