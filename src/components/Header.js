88a850a0721310310f817a5c3a7b7950ec9c6a49
Prahlad <prahladjngp@gmail.com>
2023-09-07 07:31:54 +0530

    NavLinks with Link tag - ✅ Best Approach - No Page Reload 🔥🔥🔥

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import logo from "../logo-v.png";
+import { Link } from "react-router-dom";
 
 const Heading = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
@@ -12,13 +13,13 @@ const Heading = () => {
       <div className="nav-items">
         <ul>
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <Link to="/contact">Contact</Link>
           </li>
           <li>Cart</li>
           <button
