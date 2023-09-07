0354f8ad1c9fae958b123bead34dfe5c4cc95cfd
Prahlad <prahladjngp@gmail.com>
2023-09-07 07:45:33 +0530

    🚀 Issue Resolved with React Router DOM - v6.11

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Heading from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import logo from "../logo-v.png";
-import { Link } from "react-router-dom";
 
 const Heading = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
