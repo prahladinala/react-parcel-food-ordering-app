848a78882fad3961c0993e49980efb0618f7cb74
Prahlad <prahladjngp@gmail.com>
2023-09-07 07:07:31 +0530

    Outlet - React Router DOM - Get Header and Footer on All Pages🔥🔥🔥

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Heading from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -12,7 +12,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Heading />
-      <Body />
+      <Outlet />
       <Footer />
     </div>
   );
@@ -21,15 +21,21 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
